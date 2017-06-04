7a9ca907b6d4e2bd6d797accadfecb0ad06d5579
root <root@localhost.localdomain>
2017-06-04 04:23:15 +0100

    add index page 3rd

--- hubuki.js
+++ hubuki.js
@@ -11,7 +11,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('index', {title : 'ブッキー！'});
 });
 
 app.listen(app.get('port'), function() {
